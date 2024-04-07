36885531d878d602dfb81809db6a878eca13fc7e
robjam1990 <robjam1990@live.ca>
2024-04-06 20:31:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using static System.Console;
+
+namespace TextAdventureGame
+{
+    public class Program
+    {
+        private static readonly string[] Choices = ["north", "south", "east", "west"];
+
+        private static void Run()
+        {
+            Clear();
+            WriteLine("Welcome to the text game!");
+            WriteLine($"You are in a dark room and you can go {string.Join(", ", Choices)}.");
+            string direction = ReadLine()!;
+            if (direction == "north" || direction == "south" || direction == "east" || direction == "west")
+            {
+                WriteLine("You chose to go " + direction + ".");
+            }
+            else
+            {
+                WriteLine("Invalid direction.");
+            }
+            switch (direction)
+            {
+                case "north":
+                    WriteLine("You go north and find a treasure chest.");
+                    break;
+                case "south":
+                    WriteLine("You go south and find a dragon.");
+                    break;
+                case "east":
+                    WriteLine("You go east and find a river.");
+                    break;
+                case "west":
+                    WriteLine("You go west and find a forest.");
+                    break;
+                default:
+                    WriteLine("Invalid direction.");
+                    break;
+            }
+        }
+
+        private static void Outro()
+        {
+            WriteLine("Thanks for playing the text game!");
+        }
+
+        static void Main()
+        {
+            WriteLine("Hello World!");
+            WriteLine("Would you like to play a text game?");
+            string response = ReadLine()!;
+            if (response == "yes")
+            {
+                WriteLine("Great! Let's play the text game.");
+                ReadKey(true);
+                Run();
+            }
+            else
+            {
+                WriteLine("That's too bad. Maybe next time.");
+                ReadKey(true);
+            }
+            Outro();
+        }
+    }
+}
