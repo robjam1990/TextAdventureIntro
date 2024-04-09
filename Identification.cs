d04313f70ad30b8f48ca30e341d947c553ffe8d9
robjam1990 <robjam1990@live.ca>
2024-04-08 22:19:34 -0400

    Identification with plugins

---
+++ Identification.cs
@@ -0,0 +1,86 @@
+﻿/*
+<Comments> and Suggestions section:
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    public static class Identification
+    {
+        public static string Identify(object observation)
+        {
+            if (observation is string observationString)
+            {
+                // Logic to identify the observation based on the observation string
+                // For example, you can use switch case statements or if-else conditions
+
+                switch (observationString)
+                {
+                    case "Observation A":
+                        // Identification logic for Observation A
+                        return "Identification Result A";
+                    case "Observation B":
+                        // Identification logic for Observation B
+                        return "Identification Result B";
+                    default:
+                        // Default identification logic
+                        return "Unknown Observation";
+                }
+            }
+            else
+            {
+                // Default identification logic for non-string observations
+                return "Unknown Observation";
+            }
+        }
+    }
+
+    public class Plugin()
+    {
+        private static IEnumerable<object>? plugin;
+
+        /// <summary>
+        /// Adds a plugin to the system.
+        /// </summary>
+        /// <param name="plugin">The plugin to add.</param>
+        public static void AddPlugin(object plugin)
+        {
+            // Add plugin to the list
+            Add(plugin);
+        }
+
+        private static void Add(object plugin)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Triggers plugins for a given event.
+        /// </summary>
+        /// <param name="eventName">The name of the event.</param>
+        /// <param name="data">The data associated with the event.</param>
+        public static void TriggerPlugins(string eventName, object data)
+        {
+            // Trigger plugins
+            foreach (var plugin in plugin)
+            {
+                try
+                {
+                    var method = plugin.GetType().GetMethod(eventName);
+                    if (method != null)
+                    {
+                        method.Invoke(plugin, new object[] { data }); // Invoke the method with data
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error executing plugin for event '{eventName}': {ex.Message}");
+                }
+            }
+        }
+    }
+}
